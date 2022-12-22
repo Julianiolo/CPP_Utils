1ac5a9f6574767db2b604e69528df56b80c80412
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2022-12-22 16:24:55 +0100

    moved BitArray and BitVector here

---
+++ src/bitVector.cpp
@@ -0,0 +1,24 @@
+#include "bitVector.h"
+#include <stdexcept>
+
+BitVector::BitVector(){
+
+}
+BitVector::BitVector(bsize_t size) : size(size), byteSize(size/8+(size%8==0?0:1)), data(byteSize,0){
+
+}
+bool BitVector::get(bsize_t i){
+    if(i < size)
+        std::runtime_error("Bytearray get index out of bounds");
+    return (data[i/8] & (1 << i%8)) != 0;
+}
+void BitVector::set(bsize_t i, bool val){
+    if (i / 8 >= byteSize)
+        std::runtime_error("Byte array set index out of bounds");
+    if(val){
+        data[i/8] |= (1 << i%8);
+    }
+    else{
+        data[i/8] &= ~(1 << i%8);
+    }
+}
