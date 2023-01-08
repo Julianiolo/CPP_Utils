dbe8ae700405127c3594c4a99f743aa0c5da92df
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2023-01-08 21:46:05 +0100

    added ActionManager

--- src/ActionManager.cpp
+++ src/ActionManager.cpp
@@ -1,1 +1,40 @@
 #include "ActionManager.h"
+
+void ActionManager::Action::addKey(int id) {
+	parts.push_back({ Part::Type_Key, id });
+}
+void ActionManager::Action::addMouseButton(int id) {
+	parts.push_back({ Part::Type_MouseButton, id });
+}
+
+ActionManager::Action& ActionManager::addAction(size_t id) {
+	return actions[id] = Action();
+}
+ActionManager::Action& ActionManager::getAction(size_t id) {
+	return actions[id];
+}
+
+bool ActionManager::isActionActive(size_t id, ActivationState activationState) {
+	const Action& action = getAction(id);
+
+	ActivationState testState = (activationState == ActivationState_Down || activationState == ActivationState_Pressed) ? ActivationState_Down : ActivationState_Up;
+	for (size_t i = 0; i < action.parts.size(); i++) {
+		if (!testCallB(action.parts[i].type, action.parts[i].id, testState)) {
+			return false;
+		}
+	}
+
+	if (activationState == ActivationState_Pressed || activationState == ActivationState_Released) {
+		for (size_t i = 0; i < action.parts.size(); i++) {
+			if (testCallB(action.parts[i].type, action.parts[i].id, activationState)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	return true;
+}
+
+void ActionManager::setTestCallB(const TestCallB& callB) {
+	testCallB = callB;
+}
--- src/ActionManager.h
+++ src/ActionManager.h
@@ -1,6 +1,50 @@
 #ifndef __CPPUTILS_ACTIONMANAGER_H__
 #define __CPPUTILS_ACTIONMANAGER_H__
 
+#include <cstdint>
+#include <vector>
+#include <map>
+#include <functional>
 
+class ActionManager {
+	struct Action {
+		struct Part {
+			enum {
+				Type_MouseButton=0,
+				Type_Key
+			};
+			uint8_t type;
+			int id;
+		};
+
+		std::vector<Part> parts;
+
+		void addKey(int id);
+		void addMouseButton(int id);
+	};
+
+	enum {
+		ActivationState_Down=0,
+		ActivationState_Up,
+		ActivationState_Pressed,
+		ActivationState_Released,
+	};
+	typedef uint8_t ActivationState;
+
+	typedef std::function<bool(uint8_t type, int id, ActivationState activationState)> TestCallB;
+
+private:
+	std::map<size_t, Action> actions;
+	TestCallB testCallB;
+public:
+	
+	Action& addAction(size_t id);
+	Action& getAction(size_t id);
+
+	bool isActionActive(size_t id, ActivationState activationState);
+	
+	void setTestCallB(const TestCallB& callB);
+	
+};
 
 #endif
