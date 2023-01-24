0cca4b9c48f52ff54ec47c0b6add05f871569513
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2023-01-24 12:26:39 +0100

    added omem_streambuf

--- src/StreamUtils.cpp
+++ src/StreamUtils.cpp
@@ -4,3 +4,12 @@ StreamUtils::imem_streambuf::imem_streambuf(const char* data, size_t size) {
     char* p = (char*)data;
     this->setg(p,p,p+size);
 }
+
+std::streamsize StreamUtils::omem_streambuf::xsputn(const char_type* s, std::streamsize n) {
+    buf.insert(buf.end(), s, s+n);
+    return n;
+}
+StreamUtils::omem_streambuf::int_type StreamUtils::omem_streambuf::overflow(int_type c) {
+    buf.push_back(c);
+    return 0;
+}
--- src/StreamUtils.h
+++ src/StreamUtils.h
@@ -2,10 +2,19 @@
 #define __STREAMUTILS_H__
 
 #include <streambuf>
+#include <vector>
 
 namespace StreamUtils {
     struct imem_streambuf : std::streambuf {
         imem_streambuf(const char* data, size_t size);
+    };
+    
+    struct omem_streambuf : std::streambuf {
+    public:
+        std::vector<char> buf;
+        
+        std::streamsize xsputn(const char_type* s, std::streamsize n);
+        int_type overflow(int_type c);
     };
 }
 
