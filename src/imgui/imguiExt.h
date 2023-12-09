40f456d4fc4e7639d5fa97651d997d0a8bf34f2d
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2023-12-09 03:02:50 +0100

    added missing include

--- src/imgui/imguiExt.h
+++ src/imgui/imguiExt.h
@@ -9,6 +9,9 @@
 #include "raylib.h"
 #include <string>
 #include <vector>
+#include <functional>
+
+#include "../DataUtils.h"
 
 static inline ImVec4  operator*(const ImVec4& lhs, const float rhs)     { return ImVec4(lhs.x * rhs, lhs.y * rhs, lhs.z * rhs, lhs.w * rhs); }
 static inline ImVec4  operator/(const ImVec4& lhs, const float rhs)     { return ImVec4(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs, lhs.w / rhs); }
