181556366c0ad2131268ba567457c039bf29deb8
Julianiolo <50519317+Julianiolo@users.noreply.github.com>
2024-03-09 18:50:49 +0100

    fixed CachedStringTable

--- src/comps/StringTable.h
+++ src/comps/StringTable.h
@@ -36,7 +36,7 @@ public:
 		return data.size();
 	}
 
-	inline size_t addStr(const char* str, const char* str_end = 0, bool nullTerm = true) {
+	inline StringTable::str addStr(const char* str, const char* str_end = 0, bool nullTerm = true) {
 		DU_ASSERT(str_end == 0 || str_end >= str);
 		if (str_end == 0)
 			str_end = str + std::strlen(str);
@@ -79,10 +79,100 @@ public:
 	}
 };
 
+struct _CST_string_view {
+	enum class Mode : uint8_t {
+		RawString,
+		StringTableString
+	};
+
+	const Mode mode;
+
+	struct RawString {
+		const char* s;
+		const char* s_end;
+	};
+
+	struct STString {
+		StringTable::str s;
+		const StringTable* src;
+	};
+
+	union {
+		RawString rs;
+		STString sts;
+	};
+
+	_CST_string_view(const char* str, const char* str_end) : mode(Mode::RawString), rs({ str , str_end ? str_end : str + strlen(str)}) {
+
+	}
+	_CST_string_view(StringTable::str s, StringTable* src) : mode(Mode::StringTableString), sts({ s, src }) {
+
+	}
+
+	void setSrc(const StringTable* src) {
+		DU_ASSERT(mode == Mode::StringTableString);
+		sts.src = src;
+	}
+
+	std::string_view getStr() const {
+		switch (mode) {
+			case Mode::RawString:
+				return std::string_view(rs.s, rs.s_end-rs.s);
+			case Mode::StringTableString:
+				return sts.src->getStr(sts.s);
+		}
+	}
+
+	bool operator==(const _CST_string_view& other) const {
+		return getStr() == other.getStr();
+	}
+
+	size_t hash() const {
+		return DU_HASH(getStr());
+	}
+};
+
+template<>
+struct std::hash<_CST_string_view> {
+	hash() {}
+	std::size_t operator()(const _CST_string_view& s) const noexcept {
+		return s.hash();
+	}
+};
+
 class CachingStringTable : public StringTable {
 private:
-	std::unordered_map<std::string_view, StringTable::str> cache;
+	std::unordered_map<_CST_string_view, StringTable::str> cache;
+	
+	void constructUpdatedCache(const std::unordered_map<_CST_string_view, StringTable::str>& other_cache) {
+		cache.clear();
+		cache.reserve(other_cache.size());
+		StringTable* me = static_cast<StringTable*>(this);
+		
+		for (const auto& kv : other_cache) {
+			_CST_string_view key = kv.first;
+			key.setSrc(me);
+			cache.insert({ key, kv.second });
+		}
+	}
 public:
+	CachingStringTable(const CachingStringTable& other) : StringTable(other), cache() {
+		constructUpdatedCache(other.cache);
+	}
+	CachingStringTable(CachingStringTable&& other) : StringTable(std::move(other)), cache() {
+		constructUpdatedCache(other.cache);
+	}
+
+	CachingStringTable& operator=(const CachingStringTable& other) {
+		StringTable::operator=(other);
+		constructUpdatedCache(other.cache);
+	}
+	CachingStringTable& operator=(CachingStringTable&& other) {
+		StringTable::operator=(std::move(other));
+		constructUpdatedCache(other.cache);
+		other.cache.clear();
+	}
+
 	inline void clear() {
 		cache.clear();
 		StringTable::clear();
@@ -92,7 +182,7 @@ public:
 		cache.clear();
 	}
 
-	inline size_t addStr(const char* str, const char* str_end = 0, bool nullTerm = true) {
+	inline StringTable::str addStr(const char* str, const char* str_end = 0, bool nullTerm = true) {
 		if (nullTerm == false || (data.size() > 0 && data[data.size()-1] != 0)) {  // check if previous entry wasn't yet null terminated
 			return StringTable::addStr(str, str_end, nullTerm);
 		}
@@ -101,16 +191,14 @@ public:
 		if (str_end == 0)
 			str_end = str + std::strlen(str);
 
-		const size_t str_len = str_end - str;
-
-		auto res = cache.find(std::string_view(str, str_len));
+		auto res = cache.find(_CST_string_view(str, str_end));
 		if (res != cache.end()) {
 			return res->second;
 		}
 
-		const size_t off = StringTable::addStr(str, str_end, nullTerm);
+		const StringTable::str off = StringTable::addStr(str, str_end, nullTerm);
 
-		cache[std::string_view(getStr(off), str_len)] = off;
+		cache[_CST_string_view(off, static_cast<StringTable*>(this))] = off;
 
 		return off;
 	}
